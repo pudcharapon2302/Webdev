fb0778f5d01deb80f0f5dcfb277f509ed6109dbb
Sc242_038 <pudcharapon.yi.66@ubu.ac.th>
2025-07-03 14:44:05 +0700

    initial commit

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    # id = models.AutoField()
+    name = models.CharField(max_length=200)
+    dob = models.DateField()
+    email = models.EmailField()
